1f44148602f21290724e278709610162aa76dcc1
hesrayilmaz <hesra.yilmazz@gmail.com>
2022-07-27 15:37:49 +0300

    add ladder

--- Assets/Joystick Pack/Examples/JoystickPlayerExample.cs
+++ Assets/Joystick Pack/Examples/JoystickPlayerExample.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class JoystickPlayerExample : MonoBehaviour
 {
     public float speed;
-    public VariableJoystick variableJoystick;
+    public FixedJoystick fixedJoystick;
     public Rigidbody rb;
 
+    private void Update()
+    {
+        transform.DOMove((Vector3.forward * fixedJoystick.Vertical+Vector3.right * fixedJoystick.Horizontal), 0.04f).SetRelative();
+    }
     public void FixedUpdate()
     {
-        Vector3 direction = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
-        rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        //Vector3 direction = Vector3.forward * fixedJoystick.Vertical + Vector3.right * fixedJoystick.Horizontal;
+        //rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        
     }
 }
