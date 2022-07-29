c9cadd5088a883e92d2eef35c0a8ce6269efdd52
hesrayilmaz <hesra.yilmazz@gmail.com>
2022-07-29 11:45:39 +0300

    generate levels based on given input number

--- Assets/Scripts/CharacterManager.cs
+++ Assets/Scripts/CharacterManager.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-public class CharacterController : MonoBehaviour
+public class CharacterManager : MonoBehaviour
 {
     [SerializeField] private SimpleAnimancer _animancer;
     [SerializeField] private FixedJoystick fixedJoystick;
     [SerializeField] private AudioSource _audio;
+    [SerializeField] private GameObject _platform;
+    [SerializeField] private LevelController _newLevel;
 
     [SerializeField] private string _idleAnimName = "Idle";
     [SerializeField] private float _idleAnimSpeed = 1f;
@@ -20,12 +22,12 @@ public class CharacterController : MonoBehaviour
     private bool _isClimbingUpward = false;
     private bool _isClimbingDownward = false;
     private bool _isClimbed = false;
-
+    private Vector3 _characterPos;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        _characterPos = new Vector3(64f, 420f, 450f);
     }
 
     // Update is called once per frame
@@ -33,24 +35,22 @@ public class CharacterController : MonoBehaviour
     {
         if (fixedJoystick.Vertical != 0 || fixedJoystick.Horizontal != 0)
         {
+            _isClimbed = false;
             RunAnimation();
             //transform.DORotate((Vector3.forward * fixedJoystick.Vertical + Vector3.right * fixedJoystick.Horizontal),1f);
         }
         else if (_isClimbingUpward && !_isClimbed)
         {
-            Debug.Log("1");
             ClimbAnimation();
             transform.DOMoveY(7f, 0.07f).SetRelative();
         }
         else if (_isClimbed)
         {
-            Debug.Log("2");
             _audio.Stop();
             IdleAnimation();
         }
         else if (_isClimbingDownward)
         {
-            Debug.Log("3");
             _audio.Stop();
             IdleAnimation();
             //ClimbAnimation();
@@ -60,11 +60,22 @@ public class CharacterController : MonoBehaviour
             IdleAnimation();
 
        
-        transform.DOMove((Vector3.forward * fixedJoystick.Vertical + Vector3.right * fixedJoystick.Horizontal), 0.02f).SetRelative();
+        transform.DOMove((Vector3.forward * fixedJoystick.Vertical + Vector3.right * fixedJoystick.Horizontal), 0.01f).SetRelative();
         transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation((Vector3.forward * fixedJoystick.Vertical + Vector3.right * fixedJoystick.Horizontal)), Time.deltaTime * _rotateSpeed);
 
     }
 
+    private void LateUpdate()
+    {
+        if (_isClimbed)
+        {
+            _characterPos = _newLevel.GetCurrentLevel().transform.position + new Vector3(80f, -404f, -1600f);
+            Debug.Log(_characterPos);
+            transform.position = _characterPos;
+        }
+
+    }
+    
 
     private void OnTriggerEnter(Collider other)
     {
@@ -74,6 +85,7 @@ public class CharacterController : MonoBehaviour
             _isClimbingDownward = false;
             _isClimbed = false;
             _audio.Play();
+            _newLevel.GenerateLevel();
         }
         else if (other.gameObject.tag == "LadderEnd")
         {
--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class LevelController : MonoBehaviour
 {
+    [SerializeField] private GameObject _floor;
+    [SerializeField] private int _numOfFloor;
+    private GameObject _currentLevel;
+    private int _currentNum = 0;
+    private float _yDiff = 413f;
+    private float _zDiff = 793f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _floor.transform.position = new Vector3(-16f, 411f, 1263f);
+        Instantiate(_floor);
+        //GenerateLevel();
     }
 
     // Update is called once per frame
-    void Update()
+    public void GenerateLevel()
     {
+        _currentNum += 1;
+        if (_currentNum < _numOfFloor)
+        {
+            _floor.transform.position = _floor.transform.position + new Vector3(0f, _yDiff, _zDiff);
+            _currentLevel = Instantiate(_floor);
+        }
+            
         
     }
+
+    public GameObject GetCurrentLevel()
+    {
+        return _currentLevel;
+    }
+
 }
