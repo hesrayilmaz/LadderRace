f79f0986f08fc18f320be522a3f7f3979e7a6efa
hesrayilmaz <hesra.yilmazz@gmail.com>
2022-08-03 10:30:36 +0300

    add trail renderer to bricks

--- Assets/Scripts/PickUpItems.cs
+++ Assets/Scripts/PickUpItems.cs
@@ -11,6 +11,7 @@ public class PickUpItems : MonoBehaviour
     [SerializeField] private AudioSource _pickUpAudio;
     private List<GameObject> _brickList;
     private bool _pickedUp;
+    private Vector3 _startPos, _endPos;
 
 
     // Start is called before the first frame update
@@ -28,16 +29,20 @@ public class PickUpItems : MonoBehaviour
         {
             _myBrick.gameObject.GetComponent<Rigidbody>().isKinematic = true;
 
-            //_myBrick.transform.rotation = Quaternion.LookRotation(new Vector3(0f,0f,0f));
             _myBrick.transform.localRotation = Quaternion.identity;
             //_myBrick.transform.localPosition = Vector3.zero;
             _myBrick.transform.parent = _character.transform;
-
+            
             if (_brickList.Count == 0)
                 _myBrick.transform.position = _character.transform.position;
             else
-                _myBrick.transform.position = _brickList[_brickList.Count - 1].transform.position+new Vector3(0f,1f,0f);
-
+            {
+                _startPos = _myBrick.transform.position;
+                _endPos = _brickList[_brickList.Count - 1].transform.position + new Vector3(0f, 1.1f, 0f);
+                StartCoroutine(TrailRendererProcess(_myBrick));
+            }
+                
+                //Vector3.Lerp(_brickList[_brickList.Count - 1].transform.position, _brickList[_brickList.Count - 1].transform.position+new Vector3(0f,1.1f,0f),Time.deltaTime*100f);
             _brickList.Add(_myBrick);
         }
     }
@@ -47,15 +52,27 @@ public class PickUpItems : MonoBehaviour
         if (other.gameObject.tag == "Brick")
         {
             _pickUpAudio.Play();
-            _pickedUp = true;
             _myBrick = other.gameObject;
+            
+            _pickedUp = true;
+            
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
         _pickedUp = false;
+        
+            //_myBrick.GetComponent<TrailRenderer>().emitting = false;
     }
 
+
+    IEnumerator TrailRendererProcess(GameObject go)
+    {
+        go.GetComponent<TrailRenderer>().emitting = true;
+        go.transform.position = Vector3.Lerp(_startPos, _endPos, 1f);
+        yield return new WaitForSeconds(0.7f);
+        go.GetComponent<TrailRenderer>().emitting = false;
+    }
 
 }
